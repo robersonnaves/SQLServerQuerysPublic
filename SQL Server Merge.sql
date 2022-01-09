2910891d362a70a156c7b8cac36528c51d2cce4a
Roberson Afonso Naves <robersonnaves@gmail.com>
2022-01-09 19:02:39 -0300

    Update SQL Server Merge.sql

--- SQL Server Merge.sql
+++ SQL Server Merge.sql
@@ -1,28 +1,25 @@
 USE TesteA
 GO
 
-MERGE 
-    dbo.TabelaB AS Destino
-USING 
-    (SELECT * FROM dbo.TabelaA) AS Origem ON (Origem.Id = Destino.Id) -- chaves para comparação
+MERGE dbo.TabelaB AS Destino
+USING
+(SELECT * FROM dbo.TabelaA) AS Origem
+ON (Origem.Id = Destino.Id) -- chaves para comparação
 
 -- Se o registro existe nas 2 tabelas
 WHEN MATCHED THEN
-    UPDATE SET 
-        Destino.Id = Origem.Id,
-        Destino.Nome = Origem.Nome,
-        Destino.Cidade = Origem.Cidade
-    
+    UPDATE SET Destino.Id = Origem.Id,
+               Destino.Nome = Origem.Nome,
+               Destino.Cidade = Origem.Cidade
+
 -- Se não existe na origem, apenas no destino, apaga linha da origem.
 WHEN NOT MATCHED BY SOURCE THEN
     DELETE
-    
+
 -- Se o registro não existe no destino, insere.
 WHEN NOT MATCHED BY TARGET THEN
-    INSERT
-    VALUES(Origem.Id, Origem.Nome, Origem.Cidade)
-
-OUTPUT 
-    $action, 
-    Inserted.*,
-    Deleted.*;
+    INSERT VALUES
+           (Origem.Id, Origem.Nome, Origem.Cidade)
+OUTPUT $action,
+       Inserted.*,
+       Deleted.*;
